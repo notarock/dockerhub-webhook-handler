7f02c02166f4121fb5fa6345b225ac14be0865ae
Roch D'Amour <roch.damour@gmail.com>
2019-02-12 13:09:40 -0500

    Add Vagrant test environment

---
+++ gateway.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+apt-get update
+apt-get upgrade -y
+
+# install vim.
+apt-get install -y --no-install-recommends vim
+
+# install git and curl
+sudo apt install -y  git curl golang
+curl https://get.docker.com/ | bash -
+
+service start docker
+docker network create proxy
+
+git clone https://github.com/notarock/gopdater
+cd gopdater
+mkdir sources
+git clone https://github.com/notarock/portfolio-ng sources/portfolio.ng
+cd sources/portfolio.ng
+git reset --hard f1464db63eeb20244d5538502c92653026ccad86
+bash rebuild.sh
+
+cd -
+
+echo "#!/bin/bash
+bash ../sources/rebuild.sh" > scripts/portfolio
+
+chmod +x scripts/portfolio
+
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+
+go get github.com/gorilla/mux
+go build
+
